2a83df419b0cda5a22477eb197e71b95fee8332e
gayeon7877 <gayean01@gmail.com>
2023-07-04 02:14:14 +0900

    feat:home

--- app/src/main/java/com/example/kicking/MypageVPAdapter.kt
+++ app/src/main/java/com/example/kicking/MypageVPAdapter.kt
@@ -2,7 +2,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.kicking.BoardFragment
-import com.example.kicking.HomeFragment
+
 import com.example.kicking.MycontentFragment
 
 class MypageVPAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
